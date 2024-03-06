a59df0f67905574b4274762644c91e177a8d96d3
Filip Bojovic <filip.bojovic@bwt.ee>
2024-03-06 12:52:54 +0100

    The initial commit

---
+++ reactive-oauth-demo/authorization-server/src/main/java/com/reactiveoauth2demo/authorizationserver/controllers/DemoController.java
@@ -0,0 +1,21 @@
+package com.reactiveoauth2demo.authorizationserver.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class DemoController {
+
+    @GetMapping
+    public Flux<Integer> demo() {
+        return Flux.fromIterable(List.of(1, 2, 3, 4, 5))
+                .delayElements(Duration.ofSeconds(1));
+    }
+
+}
---
+++ reactive-oauth-demo/authorization-server/src/test/java/com/reactiveoauth2demo/authorizationserver/AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactiveoauth2demo.authorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
